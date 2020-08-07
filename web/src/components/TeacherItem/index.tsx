6f15d7cade93d374566623bb4e058fefa0d15bde
Ronald Lopes <ronaldlopes@pop-os.localdomain>
2020-08-07 15:34:04 -0300

    Day 3

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,30 +2,51 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
-function TeacherItem() {
+import api from '../../services/api'
+
+
+export  interface Teacher{
+    avatar: string,
+    bio: string,
+    cost: number,
+    id: number,
+    name: string,
+    subject: string,
+    user_id: number,
+    whatsapp: string
+}
+
+interface TeacherItemProps{
+    teacher: Teacher
+}
+
+const TeacherItem:React.FC<TeacherItemProps> =({teacher})=>{
+    function createNewConnection(){
+        api.post('/connections',{
+            user_id: teacher.id
+        })
+    }
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars0.githubusercontent.com/u/23136287?s=460&u=e3fb561af6ea5bb1f5ac2ef9c6691ab0f484544f&v=4" alt="Ronald Lopes"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Ronald Lopes</strong>
-                            <span>Quimica</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
                     <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br/><br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+                        {teacher.bio}
                     </p>
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$80,00</strong>
+                            <strong>R${teacher.cost}</strong>
                         </p>
-                        <button type='button'>
+                        <a target='_blank' href={`https://wa.me/${teacher.whatsapp}`} onClick={createNewConnection}>
                             <img src={whatsappIcon} alt="whatsapp"/>
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     )
